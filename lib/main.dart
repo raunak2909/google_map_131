e80adea1196b907fa69eea29d56a4c096dfaeadd
raunakpurohit <raunak.wscubetech@gmail.com>
2023-10-21 22:34:06 +0530

    Current Location Tracking Package implementation gecoding added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -33,7 +34,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var initPos = CameraPosition(target: LatLng(19.228825, 72.854118), zoom: 15);
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  var initPos = CameraPosition(
+    target: LatLng(19.228825, 72.854118),
+    zoom: 15,
+    tilt: 16,
+  );
+
+  LatLng? myLoc;
 
   @override
   void initState() {
@@ -43,45 +53,44 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      timeLimit: Duration(seconds: 10),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Maps'),
       ),
       body: GoogleMap(
+        onMapCreated: (mController) {
+          _controller.complete(mController);
+        },
         onTap: (location) {
           print("Location: ${location.latitude}, ${location.longitude}");
         },
         initialCameraPosition: initPos,
+        mapType: MapType.hybrid,
         markers: {
           Marker(
-              infoWindow: InfoWindow(title: "Delhi"),
-              markerId: MarkerId("1"),
-              position: LatLng(28.6563, 77.2321),
-              onTap: () {
-                print("Tapped on Marker..");
-              }),
-          Marker(
-              infoWindow: InfoWindow(title: "Market"),
-              markerId: MarkerId("1"),
-              position: LatLng(19.199821, 72.842590),
-              onTap: () {
-                print("Tapped on Marker..");
-              }),
-          Marker(
-              infoWindow: InfoWindow(title: "Haryana"),
-              markerId: MarkerId("1"),
-              position: LatLng(28.408913, 77.317787),
-              onTap: () {
-                print("Tapped on Marker..");
-              }),
-          Marker(
-              infoWindow: InfoWindow(title: "Jaipur"),
-              markerId: MarkerId("1"),
-              position: LatLng(26.860830, 75.814807),
+              infoWindow: InfoWindow(title: "My Location"),
+              markerId: MarkerId("Home"),
+              position: myLoc ?? LatLng(28.6563, 77.2321),
               onTap: () {
                 print("Tapped on Marker..");
               }),
         },
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        /*circles: {
+          Circle(
+            circleId: CircleId("home_circle"),
+            center: myLoc ?? LatLng(28.6563, 77.2321),
+            fillColor: Colors.blue.withOpacity(0.5),
+            strokeWidth: 0,
+            radius: 50,
+          )
+        },*/
       ),
     );
   }
@@ -94,51 +103,72 @@ class _HomePageState extends State<HomePage> {
     if (!serviceEnabled) {
       ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Please Enable Location Services!!')));
-    }
-    else {
+    } else {
       // GPS is ON
       permission = await Geolocator.checkPermission();
       if (permission == LocationPermission.denied) {
         permission = await Geolocator.requestPermission();
         if (permission == LocationPermission.denied) {
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Please allow app to request your current location')));
-        } else if (permission == LocationPermission.deniedForever){
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('You\'ve denied this permission forever, therfore you wont be able to access this particular feature!!')));
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content:
+                  Text('Please allow app to request your current location')));
+        } else if (permission == LocationPermission.deniedForever) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text(
+                  'You\'ve denied this permission forever, therfore you wont be able to access this particular feature!!')));
         } else {
           //permission granted!!
           //getCurrentLocation();
-          getContinuousLocation();
+          getCurrentLocation();
         }
-
-
-
       } else {
         // permission already given
         //getCurrentLocation();
-        getContinuousLocation();
+        getCurrentLocation();
       }
     }
   }
 
-  void getCurrentLocation() async{
+  void getCurrentLocation() async {
+    var position = await Geolocator.getCurrentPosition();
 
-    var pos = await Geolocator.getCurrentPosition();
-
-    print("Location: ${pos.latitude}, ${pos.longitude}");
-
+    print("Location: ${position.latitude}, ${position.longitude}");
+    if (position != null) {
+      myLoc = LatLng(position.latitude, position.longitude);
+      List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+      print(placemarks);
+      print(placemarks[0].locality);
+      print(placemarks[0].name);
+      final GoogleMapController controller = await _controller.future;
+      setState(() {});
+      controller.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(target: myLoc!, tilt: 59.440717697143555, zoom: 19)));
+    }
   }
 
-  void getContinuousLocation(){
-
+  void getContinuousLocation() {
     final LocationSettings locationSettings = LocationSettings(
       accuracy: LocationAccuracy.high,
       timeLimit: Duration(seconds: 10),
     );
-    StreamSubscription<Position> positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen(
-            (Position? position) {
-          print(position == null ? 'Unknown' : '${position.latitude.toString()}, ${position.longitude.toString()}');
-        });
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) async {
+      print(position == null
+          ? 'Unknown'
+          : '${position.latitude.toString()}, ${position.longitude.toString()}');
+
+      if (position != null) {
+        myLoc = LatLng(position.latitude, position.longitude);
+        List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+        print(placemarks);
+        print(placemarks[0].locality);
+        print(placemarks[0].name);
+        final GoogleMapController controller = await _controller.future;
+        setState(() {});
+        controller.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(target: myLoc!, tilt: 59.440717697143555, zoom: 19)));
+      }
+    });
   }
 }
